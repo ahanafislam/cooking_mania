67bd84224f80195ac96042299adc5f595e4e6a40
ahanafislam <ahanafislam@gmail.com>
2022-04-03 16:25:34 +0600

    Fetch meal and display in ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Container, Row } from 'react-bootstrap';
 import Meals from '../Meals/Meals';
 
 const Home = () => {
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+            .then(res => res.json())
+            .then(data => setMeals(data.meals));
+    },[]);
+
     return (
         <Container className='mt-4'>
             <Row className='g-3'>
-                <Meals></Meals>
-                <Meals></Meals>
-                <Meals></Meals>
-                <Meals></Meals>
+                {
+                    meals.map(meal => <Meals
+                            key={meal.idMeal}
+                            meal={meal}
+                        ></Meals>)
+                }
             </Row>
         </Container>
     );
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
 import './Meals.css';
 
-const Meals = () => {
+const Meals = (props) => {
+    const {strMeal, strMealThumb, strCategory} = props.meal;
+
     return (
         <Col md={3}>
-            <Card.Img variant="top" src="https://picsum.photos/200/300" />
+            <Card.Img variant="top" src={strMealThumb} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
-                <Button className='brand-bg border-0'>Go somewhere</Button>
+                <Card.Title>{strMeal}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{strCategory}</Card.Subtitle>
+                <Card.Text></Card.Text>
+                <Button className='brand-bg border-0'>Read More</Button>
             </Card.Body>
         </Col>
     );
